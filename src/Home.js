4fa56a5fa36878f0a010ded867abbbb3442045bd
mr_bhanu__33 <115440020+Bhanuprakashvasa@users.noreply.github.com>
2023-03-01 10:54:39 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="home mt-5">
+
+      <h2 className="fw-bold text-secondary fs-1">What is teacher student management?🤷‍♀️</h2>
+      <p className="mt-3 fw-bold fs-5 text-warning">Teacher student management is the linchpin that makes teaching and learning
+        achievable in the teaching learning process. It is the teacher who plays
+        the main role in planning, organizing procedures and resources,
+        arranging the environment to maximize efficiency,
+        monitoring students' progress, anticipating potential problems.</p>
+
+      <img className="mt-3 image"
+        src='https://media.istockphoto.com/id/1148232091/photo/teacher-explaining-to-students-using-digital-tablet.jpg?s=2048x2048&w=is&k=20&c=Marl96nrtUNZyQbPKpy8_xZOF72DARmpiwjqOaIyd0U=' alt='' />
+    </div>
+  )
+}
+
+export default Home
